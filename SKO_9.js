3a5e1890389a7709890c7c3d8a3588d13c3327b3
Elijah <s3945008@student.rmit.edu.au>
2024-10-28 12:40:10 +1100

    Add files via upload

--- SKO_9.js
+++ SKO_9.js
@@ -1,8 +1,12 @@
 var elevator;
 var ellipses = [];
+let InterstateBCFont;
+let Face;
 
 function preload(){
   elevator = loadImage("data/Elevator.jpeg");
+  InterstateBCFont = loadFont("Interstate_BoldCondensed.ttf");
+  Face = loadImage("data/Face.png");
 }
 
 function setup() {
@@ -13,16 +17,16 @@ function setup() {
     { x: width / 1.1321, y: height / 1.789, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-3/" },
     { x: width / 1.1321, y: height / 2, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-5/" },
     { x: width / 1.1321, y: height / 2.27, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-7/" },
-    { x: width / 1.1321, y: height / 2.625, w: width / 40, h: height / 22, url: "https://example.com/6" },
-    { x: width / 1.1321, y: height / 3.11, w: width / 40, h: height / 22, url: "https://example.com/7" },
+    { x: width / 1.1321, y: height / 2.625, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-9/" },
+    { x: width / 1.1321, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-11/" },
 
     { x: width / 1.0925, y: height / 1.475, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-0/" },
     { x: width / 1.0925, y: height / 1.617, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-2/" },
     { x: width / 1.0925, y: height / 1.789, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-4/" },
     { x: width / 1.0925, y: height / 2, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-6/" },
     { x: width / 1.0925, y: height / 2.27, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-8/" },
-    { x: width / 1.0925, y: height / 2.625, w: width / 40, h: height / 22, url: "https://example.com/13" },
-    { x: width / 1.0925, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/The-Zipped-Face-Final/" }
+    { x: width / 1.0925, y: height / 2.625, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-10/" },
+    { x: width / 1.0925, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-12/" },
   ];
 }
 
@@ -30,12 +34,18 @@ function draw() {
   background("white");
   image(elevator, 0, 0, windowWidth, windowHeight);
 
+  imageMode(CENTER);
+
+  image(Face, width / 2, height / 1.5, width / 2, height / 2);
+
+  imageMode(CORNER);
+
   strokeWeight(width / 550);
   noFill();
 
   for (let i = 0; i < ellipses.length; i++) {
     let e = ellipses[i];
-    if (i === 0) {
+    if (i === 5) {
       stroke(0, 255, 0);
     } else if (i === 1) {
       stroke(255, 0, 0);
@@ -44,6 +54,33 @@ function draw() {
     }
     ellipse(e.x, e.y, e.w, e.h);
   }
+
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth / 50);
+  textFont(InterstateBCFont); 
+  noStroke();
+  fill(0);
+  text(
+    "Codewords Creative Coding and Typography",
+    width / 2.5,
+    height / 10,
+    width / 5, // Specify the width for wrapping the text
+    height / 12,  // Specify the height for wrapping the text
+  );
+
+  // Display text in the middle of the screen
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth / 115);
+  textFont(InterstateBCFont); 
+  noStroke();
+  fill(0); 
+  text(
+    "During week 9 I created the assets for my major project using Illustrator. The assets included, face, mouth, zip and background. I decided to use Illustrator because it's a software that I'm very used to so it helped cut my work time down so I could focus on the functions of the project.",
+    width / 2.77,
+    height / 9,
+    width / 3.5, // Specify the width for wrapping the text
+    height / 4  // Specify the height for wrapping the text
+  );
 }
 
 function mousePressed() {
